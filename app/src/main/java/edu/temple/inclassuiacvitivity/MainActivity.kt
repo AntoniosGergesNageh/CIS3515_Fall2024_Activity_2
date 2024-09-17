2a274221ea5d8305b55900f2db3d0f0f63cfcf17
t00nny <129798251+t00nny@users.noreply.github.com>
2024-09-17 15:20:02 -0400

    all

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -14,11 +14,24 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100 */
-
+        val numberArray = ArrayList<Int>()
+        for (i in 5..100 step 5) {
+            numberArray.add(i) //
+        }
         /* TODO Step 3: Create adapter to display items from array in Spinner */
-
-
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberArray)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
         /* TODO Step 4: Change TextView's text size to the number selected in the Spinner */
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedNumber = parent.getItemAtPosition(position) as Int
+                displayTextView.textSize = selectedNumber.toFloat()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+
+            }
 
         }
-    }
+    }}
